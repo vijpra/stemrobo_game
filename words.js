3f7588a393275d875e7e61a112e2eb1b0d71381f
vijpra <93149661+vijpra@users.noreply.github.com>
2023-06-05 16:09:56 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "arduino",
+        hint: "It has 14 digital I/O, 6 analog inputs, 16 MHz"
+    },
+    {
+        word: "tinkercad",
+        hint: "An online platform for making 3D designs"
+    },
+    {
+        word: "ultrasonic",
+        hint: "used for measuring distance"
+    },
+    {
+        word: "LDR",
+        hint: "Used for measuring the inrensity of light"
+    },
+    {
+        word: "1023",
+        hint: "Maximun value of analog pin in arduino"
+    },
+    {
+        word: "microcntroller",
+        hint: "A smart device that design for a specific task"
+    },
+    {
+        word: "micro-processor",
+        hint: "A device used for multitasking"
+    },
+    {
+        word: "thomas-edison",
+        hint: "inventd bulb"
+    },
+    {
+        word: "registor",
+        hint: "it has some color-band on it"
+    },
+    {
+        word: "transistor",
+        hint: "base component for IC"
+    },
+    {
+        word: "capacitor",
+        hint: "Used for store the charge"
+    },
+    {
+        word: "SR-04",
+        hint: "this is the model name of ultrasonic"
+    },
+    {
+        word: "arry",
+        hint: "Collection of same data types under one name"
+    },
+    {
+        word: "current",
+        hint: "flow of electrons"
+    },
+    {
+        word: "ohm",
+        hint: "unit of resistence "
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "USB",
+        hint: "Used for transport data"
+    },
+    {
+        word: "",
+        hint: "best "
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+]
